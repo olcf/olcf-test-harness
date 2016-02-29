ab33bceb5931a9edf73c5cb3f2b7b1f30f4a611d
Arnold N. Tharrington <arnoldt@ornl.gov>
2016-02-29 11:54:38 -0500

    More work on implenting the first unit test.

--- test/src/test_runtests.py
+++ test/src/test_runtests.py
@@ -10,7 +10,6 @@ from bin import runtests
 class Test_runtests(unittest.TestCase):
     """ Tests for main program runtests.py """
     
-    
     def setUp(self):
         """ Set ups to run a basic harness tests. """
 
@@ -23,25 +22,27 @@ class Test_runtests(unittest.TestCase):
 
         # Scratch space for running unit tests.
         my_member_work = os.getenv("MEMBERWORK")
-        my_path_to_sspace = os.path.join(my_member_work,"stf006","Harness_Unit_Testing_Scratch_Space")
+        my_path_to_sspace = os.path.join(my_member_work,"Harness_Unit_Testing_Scratch_Space")
         os.putenv("RGT_PATH_TO_SSPACE",my_path_to_sspace)
 
         # Path to environmental variables file. 
-        my_rgt_env_file='/ccs/home/arnoldt/Harness_Unit_Testing/rgt_environmental_variables.bash.x'
-        os.putenv("RGT_ENVIRONMENTAL_FILE",my_rgt_env_file)
+        my_user_name = os.getenv("USER")
+        my_home_directory = os.getenv("HOME")
 
         # Path to my input file directory.
-        my_rgt_input_diretcory = "/ccs/home/arnoldt/Harness_Unit_Testing_Input"
+        my_rgt_input_directory = os.path.join(my_home_directory,"Harness_Unit_Testing_Input")
 
         # File name of rgt input file.
         my_rgt_input_file_name = "rgt.input"
+     
+        # The path to the rgt module file
+        my_rgt_module_file = os.getenv("RGT_NCCS_TEST_HARNESS_MODULE")
 
-      
-        # Create the input directory along with its input files. 
+        # Create the input directory along with the input files. 
         self.__createInputDirectoryAndFiles(my_path_to_sspace,
-                                    my_rgt_env_file,
-                                    my_rgt_input_diretcory,
-                                    my_rgt_input_file_name)
+                                            my_rgt_input_directory,
+                                            my_rgt_input_file_name,
+                                            my_rgt_module_file)
 
     def tearDown(self):
         """ Stud doc for tear down """
@@ -55,27 +56,95 @@ class Test_runtests(unittest.TestCase):
         
         self.assertEqual(101,101,"Command line arguments good.")
 
-    def __createInputDirectoryAndFiles(self,
-                               path_to_scratch_space,
-                               path_to_rgt_env_file,
-                               path_to_input_directory,
-                               rgt_input_file_name):
+    def __createInputDirectoryAndFiles(
+        self,
+        path_to_scratch_space,
+        path_to_input_directory,
+        rgt_input_file_name,
+        path_to_module_file):
         
         # Create the input directory.
         if not os.path.isdir(path_to_input_directory):
             os.makedirs(path_to_input_directory)
 
         # Create the rgt environmental file.
-        self.__createRgtEnvFile(path_to_rgt_env_file,
-                                path_to_input_directory)
+        self.__createRgtEnvFile(path_to_input_directory,
+                                path_to_scratch_space,
+                                path_to_module_file)
 
-    def __createRgtEnvFile(self,
-                           path_to_rgt_env_file,
-                           path_to_input_directory):
+        # Create the rgt input file.
+        self.__createRgtInputFile(path_to_input_directory)
+
+    def __createRgtEnvFile(
+        self,
+        path_to_input_directory,
+        path_to_scratch_space,
+        path_to_module_file):
+
+        # Define the path to the rgt environmental variables
+        # file that will be in the input directory.
+        rgt_env_file_name = "rgt.environmental_variables.sh"
+        test_rgt_env_file_path = os.path.join(path_to_input_directory,
+                                              rgt_env_file_name)
+
+        # Define a comment format 
+        comment_frmt =  "#---------------------------------------------------------------\n"
+        comment_frmt += "# {rgt_comment:<60}"
+        comment_frmt += "{rgt_space:<1}-\n"
+        comment_frmt += "#---------------------------------------------------------------\n"
+
+        #Define a export environmental variable format.
+        export_frmt =  "{rgt_variable}={rgt_variable_value}\n"
+        export_frmt += "export {rgt_variable}\n\n"
+        
+        # Open file for writing.
+        rgt_file_obj = open(test_rgt_env_file_path,"w")
+
+        # Write the PBS job account id export lines to file.
+        pbs_job_comment = comment_frmt.format(rgt_comment="JOB ACCOUNT ID",
+                                              rgt_space=" ")
+        rgt_file_obj.write(pbs_job_comment)
+
+        pbs_env = export_frmt.format(rgt_variable="RGT_PBS_JOB_ACCNT_ID",
+                                     rgt_variable_value="'STF006'")
+        rgt_file_obj.write(pbs_env)
+
+        # Write the path to scratch space to file.
+        scratch_space_comment = comment_frmt.format(rgt_comment="Absolute path to scratch space",
+                                                    rgt_space=" ")
+        rgt_file_obj.write(scratch_space_comment)
+
+        scratch_space_env = export_frmt.format(rgt_variable="RGT_PATH_TO_SSPACE",
+                                               rgt_variable_value="'" + path_to_scratch_space + "'")
+
+        rgt_file_obj.write(scratch_space_env)
+
+        # Write the path to module to load.
+        module_comment = comment_frmt.format(rgt_comment="Name of test harness module to load",
+                                             rgt_space=" ")
+
+        rgt_file_obj.write(module_comment)
+
+        module_env = export_frmt.format(rgt_variable="RGT_NCCS_TEST_HARNESS_MODULE",
+                                        rgt_variable_value="'" + path_to_module_file + "'")
+
+        rgt_file_obj.write(module_env)
+
+        # Close file.
+        rgt_file_obj.close()
 
 
-        print("Creating rgt environmental file.")
+    def __createRgtInputFile(
+        self,
+        path_to_input_directory):
 
+        rgt_input_file_name = "rgt.input"
+        test_rgt_input_file_path = os.path.join(path_to_input_directory,
+                                                rgt_input_file_name )
+
+        rgt_file_obj = open(test_rgt_input_file_path,"w")
+        rgt_file_obj.write("Stud for writing rgt input file data.")
+        rgt_file_obj.close()
 
 if __name__ == "__main__":
     unittest.main()
