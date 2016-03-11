f6f825b0f9a9c62a50fd0391d3cde8db68b3797c
Veronica G. Vergara Larrea <vergaravg@ornl.gov>
2016-03-11 14:11:55 -0500

    read environment variables for machine name and scheduler type

--- src/machine_factory.py
+++ src/machine_factory.py
@@ -1,14 +1,24 @@
+import os
 from cray_xk7 import CrayXK7
+from ibm_power8 import IBMpower8
+
 
 class MachineFactory:
 
     @staticmethod
-    def create_machine(machine_name=None,
-                       machine_type=None,
-                       scheduler_type=None):
-        print("Creating machine")
+    def create_machine():
+        rgt_machine_name = os.environ["RGT_MACHINE_NAME"]
+        rgt_scheduler_type = os.environ["RGT_SCHEDULER_TYPE"]
+        print("Creating machine "+str(rgt_machine_name)+" with scheduler "+str(rgt_scheduler_type))
 
-    def __init__(self,name,machine,scheduler):
+        tmp_machine = None
+        if rgt_machine_name == "Crest":
+            tmp_machine = IBMpower8(name=rgt_machine_name,scheduler=rgt_scheduler_type)
+        else:
+            print("Machine name does not exit. Good bye!")
+
+        return tmp_machine
+
+    def __init__(self):
         pass
-
 
