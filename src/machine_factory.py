2beecd614f6876599d44f6cb05fa244c68abf200
Veronica G. Vergara Larrea <vergaravg@ornl.gov>
2016-03-11 15:50:29 -0500

    modified the machine factory to get the job launcher from the environment variable

--- src/machine_factory.py
+++ src/machine_factory.py
@@ -9,6 +9,7 @@ class MachineFactory:
     def create_machine():
         rgt_machine_name = os.environ.get("RGT_MACHINE_NAME")
         rgt_scheduler_type = os.environ.get("RGT_SCHEDULER_TYPE")
+        rgt_jobLauncher_type = os.environ.get("RGT_JOBLAUNCHER_TYPE")
 
         if rgt_machine_name == None:
             print('No machine name provided. Please set the RGT_MACHINE_NAME variable'.format(rgt_machine_name))
@@ -16,11 +17,14 @@ class MachineFactory:
         if rgt_scheduler_type == None:
             print('No scheduler type provided. Please set the RGT_SCHEDULER_TYPE variable'.format(rgt_scheduler_type))
 
-        print("Creating machine "+str(rgt_machine_name)+" with scheduler "+str(rgt_scheduler_type))
+        if rgt_jobLauncher_type == None:
+            print('No scheduler type provided. Please set the RGT_JOBLAUNCHER_TYPE variable'.format(rgt_jobLauncher_type))
+
+        print("Creating machine "+str(rgt_machine_name)+" with scheduler "+str(rgt_scheduler_type)+" with job launcher "+str(rgt_jobLauncher_type))
 
         tmp_machine = None
         if rgt_machine_name == "Crest":
-            tmp_machine = IBMpower8(name=rgt_machine_name,scheduler=rgt_scheduler_type)
+            tmp_machine = IBMpower8(name=rgt_machine_name,scheduler=rgt_scheduler_type,jobLauncher=rgt_jobLauncher_type)
         else:
             print("Machine name does not exist. Good bye!")
 
