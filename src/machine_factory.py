2ee9279d64d28af438ebf9687df7efcc62096f16
Veronica G. Vergara Larrea <vergaravg@ornl.gov>
2016-03-11 15:35:35 -0500

    added checks to see if RGT env variables are set

--- src/machine_factory.py
+++ src/machine_factory.py
@@ -7,15 +7,22 @@ class MachineFactory:
 
     @staticmethod
     def create_machine():
-        rgt_machine_name = os.environ["RGT_MACHINE_NAME"]
-        rgt_scheduler_type = os.environ["RGT_SCHEDULER_TYPE"]
+        rgt_machine_name = os.environ.get("RGT_MACHINE_NAME")
+        rgt_scheduler_type = os.environ.get("RGT_SCHEDULER_TYPE")
+
+        if rgt_machine_name == None:
+            print('No machine name provided. Please set the RGT_MACHINE_NAME variable'.format(rgt_machine_name))
+
+        if rgt_scheduler_type == None:
+            print('No scheduler type provided. Please set the RGT_SCHEDULER_TYPE variable'.format(rgt_scheduler_type))
+
         print("Creating machine "+str(rgt_machine_name)+" with scheduler "+str(rgt_scheduler_type))
 
         tmp_machine = None
         if rgt_machine_name == "Crest":
             tmp_machine = IBMpower8(name=rgt_machine_name,scheduler=rgt_scheduler_type)
         else:
-            print("Machine name does not exit. Good bye!")
+            print("Machine name does not exist. Good bye!")
 
         return tmp_machine
 
