4b576315cf18de01a6291cc7f3ad3621abb88ce6
Veronica G. Vergara Larrea <vergaravg@ornl.gov>
2016-03-10 23:13:15 -0500

    added a method to print scheduler information and modified constructor

--- src/base_scheduler.py
+++ src/base_scheduler.py
@@ -11,20 +11,35 @@ class BaseScheduler:
 
     Attributes:
         name: string representing the scheduler's name
-        jobLauncher: an object of the BaseJobLauncher class
 
     Methods:
-        print_scheduler_info:
-        print_job_launcher_info:
         get_scheduler_name:
+        print_scheduler_info:
     """
     
-    def __init__(self,name,jobLauncher):
+    def __init__(self,name,submitCmd,statusCmd,deleteCmd,
+                 walltimeOpt,numTasksOpt,jobNameOpt):
         self.__name = name
-        self.__jobLauncher = jobLauncher
+        self.__submitCmd = submitCmd
+        self.__statusCmd = statusCmd
+        self.__deleteCmd = deleteCmd
+        self.__walltimeOpt = walltimeOpt
+        self.__numTasksOpt = numTasksOpt
+        self.__jobNameOpt = jobNameOpt
+
+    def get_scheduler_name(self):
+        return self.__name
 
     def print_scheduler_info(self):
-        print(str(self.__jobLauncher))
+        print("--------------------------------------")
+        print("Scheduler = " + self.__name)
+        print("Submit command = " + self.__submitCmd)
+        print("Status command = " + self.__statusCmd)
+        print("Delete command = " + self.__deleteCmd)
+        print("Walltime Option = " + self.__walltimeOpt)
+        print("Number of tasks Option = " + self.__numTasksOpt)
+        print("Job Name Option = " + self.__jobNameOpt)
+        print("--------------------------------------")
 
 if __name__ == "__main__":
     print "This is the BaseScheduler class!"
