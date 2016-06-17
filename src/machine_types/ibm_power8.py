e88fecef06ec4f7a0a24be5110961a5bd6cb0c3f
verolero86 <verolero86@gmail.com>
2016-06-17 15:20:03 -0400

    added src and tests directories back

---
+++ src/machine_types/ibm_power8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# Author: Veronica G. Vergara L.
+#
+#
+
+from base_machine import BaseMachine
+
+class IBMpower8(BaseMachine):
+    
+    def __init__(self,name='IBM Power8',scheduler=None,jobLauncher=None,
+                 numNodes=0,numSocketsPerNode=0,numCoresPerSocket=0):
+        BaseMachine.__init__(self,name,scheduler,jobLauncher,numNodes,
+                             numSocketsPerNode,numCoresPerSocket)
+
+if __name__ == "__main__":
+    print('This is the IBM Power8 class')
---
+++ tests/test_classes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from machine_types.cray_xk7 import CrayXK7 
+from machine_types.ibm_power8 import IBMpower8
+from machine_types.machine_factory import MachineFactory
+from machine_types.lsf import LSF
+from machine_types.pbs import PBS
+from machine_types.aprun import Aprun
+from machine_types.poe import Poe
+
+#my_machine = CrayXK7('Chester','PBS','aprun',80,1,16)
+#print my_machine.get_machine_name()
+#my_machine.print_machine_info()
+#
+#my_machine = IBMpower8('Crest','LSF','poe',4,1,22)
+#print my_machine.get_machine_name()
+#my_machine.print_machine_info()
+
+#fc = MachineFactory.create_machine('Chester','XK7','PBS')
+fc = MachineFactory.create_machine()
+print(fc)
+
+#my_scheduler = LSF()
+#print my_scheduler.get_scheduler_name()
+#my_scheduler.print_scheduler_info()
+#my_scheduler = PBS()
+#print my_scheduler.get_scheduler_name()
+#my_scheduler.print_scheduler_info()
+#
+#my_job_launcher = Aprun()
+#my_job_launcher.print_jobLauncher_info()
+#
+#my_job_launcher = Poe()
+#my_job_launcher.print_jobLauncher_info()
