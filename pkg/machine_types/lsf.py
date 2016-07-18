e3f530faae6e3d44752834bb97efec8759f500b4
Veronica G. Vergara Larrea <vergaravg@ornl.gov>
2016-07-18 15:18:16 -0400

    added a function to submit a job from the LSF scheduler

--- pkg/machine_types/lsf.py
+++ pkg/machine_types/lsf.py
@@ -5,6 +5,9 @@
 #
 
 from .base_scheduler import BaseScheduler
+import shlex
+import subprocess
+import re
 
 class LSF(BaseScheduler):
 
@@ -22,6 +25,41 @@ class LSF(BaseScheduler):
         BaseScheduler.__init__(self,self.__name,self.__submitCmd,self.__statusCmd,
                                self.__deleteCmd,self.__walltimeOpt,self.__numTasksOpt,
                                self.__jobNameOpt,self.__templateFile)
+
+    def submit_job(self,batchfilename):
+        print("Submitting job from LSF class using batchfilename " + batchfilename)
+        qcommand = self.__submitCmd
+        args = shlex.split(qcommand)
+        temp_stdout = "t1.out"
+        temp_stderr = "t1.err"
+
+        submit_stdout = open(temp_stdout,"w")
+        submit_stderr = open(temp_stderr,"w")
+        jobfileobj = open(batchfilename,"r")
+        p = subprocess.Popen(args,stdout=submit_stdout,stderr=submit_stderr,stdin=jobfileobj)
+        jobfileobj.close()
+
+        p.wait()
+
+        submit_stdout.close()
+        submit_stderr.close()
+
+        submit_stdout = open(temp_stdout,"r")
+        records = submit_stdout.readlines()
+        submit_stdout.close()
+
+        print("records = ")
+        print(records)
+
+        print("Extracting LSF jobID from LSF class")
+        jobid_pattern = re.compile('\d+')
+        print("jobid_pattern = ")
+        print(jobid_pattern)
+        print("jobid_pattern.findall = ")
+        jobid = jobid_pattern.findall(records[0])[0]
+        print(jobid)
+
+        return jobid
 
 if __name__ == '__main__':
     print('This is the LSF scheduler class')
