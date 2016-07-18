7b074e95d10942b676c9110fb0619ad933a13d8b
Veronica G. Vergara Larrea <vergaravg@ornl.gov>
2016-07-17 20:13:14 -0400

    read and store individual test parameters from test input

--- pkg/machine_types/ibm_power8.py
+++ pkg/machine_types/ibm_power8.py
@@ -2,10 +2,10 @@
 #
 # Author: Veronica G. Vergara L.
 #
-#
 
 from .base_machine import BaseMachine
 import os
+import re
 
 class IBMpower8(BaseMachine):
     
@@ -16,10 +16,109 @@ class IBMpower8(BaseMachine):
         self.__rgt_test_input = None
 
     def read_rgt_test_input(self):
+        total_processes = None
+        processes_per_node = None
+        processes_per_socket = None
+        jobname = None
+        batchqueue = None
+        walltime = None
         if os.path.isfile(self.get_rgt_input_file_name()):
             print("Reading input file from Power8")
+
+            # Read the required parameters from RGT test input
+            total_processes_pattern = "total_processes"
+            processes_per_node_pattern = "processes_per_node"
+            processes_per_socket_pattern = "processes_per_socket"
+            jobname_pattern = "jobname"
+            batchqueue_pattern = "batchqueue"
+            walltime_pattern = "walltime"
+            delimiter = "="
+
+            fileobj = open(self.get_rgt_input_file_name())
+            filerecords = fileobj.readlines()
+            fileobj.close()
+
+            # Find the number of total processes for the test
+            temp_re = re.compile(total_processes_pattern + "$")
+            for record in filerecords:
+                words = record.split(delimiter)
+                words[0] = words[0].strip().lower()
+                if temp_re.match(words[0]):
+                    total_processes = int(words[1])
+                    break
+            if total_processes:
+                print("Found total_processes is " + str(total_processes) + " in IBM Power 8 machine")
+            else:
+                print("No total_processes requested in IBM Power 8 machine")
+
+            # Find the number of processes per node the test
+            temp_re = re.compile(processes_per_node_pattern + "$")
+            for record in filerecords:
+                words = record.split(delimiter)
+                words[0] = words[0].strip().lower()
+                if temp_re.match(words[0]):
+                    processes_per_node = int(words[1])
+                    break
+            if processes_per_node:
+                print("Found processes_per_node is " + str(processes_per_node) + " in IBM Power 8 machine")
+            else:
+                print("No processes_per_node requested in IBM Power 8 machine")
+
+            # Find the number of processes per socket the test
+            temp_re = re.compile(processes_per_socket_pattern + "$")
+            for record in filerecords:
+                words = record.split(delimiter)
+                words[0] = words[0].strip().lower()
+                if temp_re.match(words[0]):
+                    processes_per_socket = int(words[1])
+                    break
+            if processes_per_socket:
+                print("Found processes_per_socket is " + str(processes_per_socket) + " in IBM Power 8 machine")
+            else:
+                print("No processes_per_socket requested in IBM Power 8 machine")
+
+
+            # Find the job name the test
+            temp_re = re.compile(jobname_pattern + "$")
+            for record in filerecords:
+                words = record.split(delimiter)
+                words[0] = words[0].strip().lower()
+                if temp_re.match(words[0]):
+                    jobname = words[1].strip('\n')
+                    break
+            if jobname:
+                print("Found jobname is " + jobname + " in IBM Power 8 machine")
+            else:
+                print("No jobname provided in IBM Power 8 machine")
+
+            # Find the queue to use for the test
+            temp_re = re.compile(batchqueue_pattern + "$")
+            for record in filerecords:
+                words = record.split(delimiter)
+                words[0] = words[0].strip().lower()
+                if temp_re.match(words[0]):
+                    batchqueue = words[1].strip('\n')
+                    break
+            if batchqueue:
+                print("Found batchqueue is " + batchqueue + " in IBM Power 8 machine")
+            else:
+                print("No batchqueue provided in IBM Power 8 machine")
+
+            # Find the walltime to use for the test
+            temp_re = re.compile(walltime_pattern + "$")
+            for record in filerecords:
+                words = record.split(delimiter)
+                words[0] = words[0].strip().lower()
+                if temp_re.match(words[0]):
+                    walltime = int(words[1])
+                    break
+            if walltime:
+                print("Found walltime is " + str(walltime) + " in IBM Power 8 machine")
+            else:
+                print("No walltime provided in IBM Power 8 machine")
+
         else:
-            print("No input found. Provide your own scripts")
+            print("No input found. Provide your own build, submit, check, and report scripts")
 
     def make_batch_script(self):
         print("Making batch script for Power8")
