f8356dba845cd6ed550f7d0cd514a98e03d392c1
Arnold N. Tharrington <arnoldt@ornl.gov>
2016-09-25 05:14:55 -0400

    More refactoring.

---
+++ nccs_test_harness_kepler_upgrade_0_00/libraries/repositories/common_repository_utility_functions.py
@@ -0,0 +1,55 @@
+import sys
+import shutil
+import os
+import subprocess
+import contextlib
+import io
+import tempfile
+
+# NCCS Tesst Harness packages
+
+def run_as_subprocess_command(cmd):
+    """ Runs the command in the string cmd by subprocess.
+
+            :param cmd: A string containing the command to run
+            :type cmd: string
+    """
+    # Run the command and write the command's 
+    # stderr and stdout results to 
+    # unique temp files. The stdout tempfile has only one record which 
+    # is searched for true or false. If true is found then
+    # sparse checkout is enabled, otherwise sparse checkouts are
+    # not enabled and we exit program.
+    exit_status = 0
+    with tempfile.NamedTemporaryFile("w",delete=False) as tmpfile_stdout:
+        with tempfile.NamedTemporaryFile("w",delete=False) as tmpfile_stderr:
+            exit_status = None
+            message = None
+            try:
+                exit_status = subprocess.check_call(cmd,
+                                                    shell=True,
+                                                    stdout=tmpfile_stdout,
+                                                    stderr=tmpfile_stderr)
+            except subprocess.CalledProcessError as exc :
+                exit_status = 1
+                message = "Error in subprocess command: " + exc.cmd
+            except:
+                exit_status = 1
+                message = "Unexpected error in command! " + cmd
+    
+            # Close the file objects of the temporary files.
+            tmpfile_stdout_path = tmpfile_stdout.name
+            tmpfile_stderr_path = tmpfile_stderr.name
+            tmpfile_stdout.close()
+            tmpfile_stderr.close()
+    
+            # Remove the temporary files if the subprocess fails.
+            if  exit_status > 0:
+                os.remove(tmpfile_stdout_path) 
+                os.remove(tmpfile_stderr_path) 
+                sys.exit(message)
+    
+            # Remove the temporary files before we return.
+            os.remove(tmpfile_stdout_path) 
+            os.remove(tmpfile_stderr_path) 
+    return
