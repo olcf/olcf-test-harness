56e575ce6d04e36fd9b859d39cef4209e450e34d
Veronica G. Vergara Larrea <vergaravg@ornl.gov>
2016-09-27 14:54:31 -0400

    cleaning up debugging output

--- nccs_test_harness_kepler_upgrade_0.00/machine_types/lsf.py
+++ nccs_test_harness_kepler_upgrade_0.00/machine_types/lsf.py
@@ -49,17 +49,17 @@ class LSF(BaseScheduler):
         records = submit_stdout.readlines()
         submit_stdout.close()
 
-        print("records = ")
-        print(records)
+        #print("records = ")
+        #print(records)
 
-        print("Extracting LSF jobID from LSF class")
+        #print("Extracting LSF jobID from LSF class")
         jobid_pattern = re.compile('\d+')
-        print("jobid_pattern = ")
-        print(jobid_pattern)
-        print("jobid_pattern.findall = ")
+        #print("jobid_pattern = ")
+        #print(jobid_pattern)
+        #print("jobid_pattern.findall = ")
         jobid = jobid_pattern.findall(records[0])[0]
         self.set_job_id(jobid)
-        print(self.get_job_id())
+        print("LSF jobID = ",self.get_job_id())
 
         return p.returncode
 
