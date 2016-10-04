de3f6e76c5a79ab7712a248782606e3210b7c464
Veronica G. Vergara Larrea <vergaravg@ornl.gov>
2016-10-04 12:37:06 -0400

    added python3 since it is the python convention and otherwise the harness does not work on summitdev

--- nccs_test_harness_kepler_upgrade_0_00/bin/run_me.py
+++ nccs_test_harness_kepler_upgrade_0_00/bin/run_me.py
@@ -1,4 +1,4 @@
-#!  /usr/bin/env python
+#!  /usr/bin/env python3
 '''
 Created on Jan 16, 2013
 
--- nccs_test_harness_kepler_upgrade_0_00/bin/runtests.py
+++ nccs_test_harness_kepler_upgrade_0_00/bin/runtests.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 from libraries import input_files
 from libraries import regression_test
 
