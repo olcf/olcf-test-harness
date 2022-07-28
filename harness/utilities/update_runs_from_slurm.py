941a5ebbb292bd7b6c044149556bbb1ce7ae47b2
Nick Hagerty <hagertynl@ornl.gov>
2022-07-28 17:21:03 -0400

    Updated update_runs_from_slurm.py to run sacct in batched mode. No batch size implemented yet. Planning to split into batches of 100 jobs

--- harness/utilities/update_runs_from_slurm.py
+++ harness/utilities/update_runs_from_slurm.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 # Author: Nick Hagerty
-# Date modified: 07-25-2022
+# Date modified: 07-28-2022
 ################################################################################
 # Purpose:
 #   Only intended for SLURM systems.
@@ -19,67 +19,43 @@ import requests
 import subprocess
 import urllib.parse
 from datetime import datetime
+import argparse
+
+
+
+# Initialize argparse ##########################################################
+parser = argparse.ArgumentParser(description="Updates harness runs in InfluxDB with SLURM data")
+parser.add_argument('--time', '-t', default=['7d'], nargs=1, action='store', help="How far back to look for jobs in Influx (ex: 1h, 2d). 'none' disables this filter.")
+parser.add_argument('--user', '-u', default=[f"{os.environ['USER']}"], nargs=1, action='store', help="Specifies the UNIX user to update jobs for.")
+parser.add_argument('--machine', '-m', default=['unknown'], nargs=1, action='store', help="Specifies the machine to look for jobs for. Setting a wrong machine may lead to SLURM job IDs not being found.")
+parser.add_argument('--app', nargs=1, action='store', help="Specifies the app to update jobs for.")
+parser.add_argument('--test', nargs=1, action='store', help="Specifies the test to update jobs for.")
+parser.add_argument('--runtag', nargs=1, action='store', help="Specifies the runtag to update jobs for.")
+################################################################################
 
 # Global URIs and Tokens #######################################################
 from harness_keys import post_influx_uri, get_influx_uri, influx_token
 ################################################################################
 
 # Parse command-line arguments #################################################
-if len(sys.argv) == 2 and 'help' in sys.argv[1]:
-    print(f"Usage: ./update_runs_from_slurm.py [args]")
-    print(f"\t--time=<timestring>    : How far back to look for jobs in Influx. 'none' disables this filter.")
-    print(f"\t--user=<username>      : Specifies the user to update jobs for.")
-    print(f"\t--machine=<machine>    : Specifies the machine to look for jobs for.")
-    print(f"\t                         WARNING: setting a wrong machine may lead to SLURM job IDs not being found.")
-    print(f"\t--app=<app>            : Specifies the app to update jobs for.")
-    print(f"\t--test=<test>          : Specifies the test to update jobs for.")
-    print(f"\t--runtag=<runtag>      : Specifies the runtag to update jobs for.")
-    sys.exit(0)
+args = parser.parse_args()
 
-current_machine = 'unknown'
-time_filter_length = '7d'   # default 7 days
-# default filter to the current user
-command_line_filters = {
-    'user': os.environ['USER']
-}
-allowed_args = ['user', 'app', 'test', 'runtag']
-
-usage = './update_runs_from_slurm.py --machine=<machinename> --time=<timestring (ex: 1d, none)>'
-current_arg_index = 1
-while current_arg_index < len(sys.argv):
-    if sys.argv[current_arg_index].startswith('--machine'):
-        arg = sys.argv[current_arg_index].split('=')
-        current_machine = arg[1]
-        print(f"Current machine identified as {current_machine} by command-line flag")
-    elif sys.argv[current_arg_index].startswith('--time'):
-        arg = sys.argv[current_arg_index].split('=')
-        if arg[1] == 'none':
-            time_filter_length = None
-        elif not (arg[1].endswith('h') or arg[1].endswith('d') or arg[1].endswith('m')):
-            print(f"Unrecognized time filter length: {arg[1]}")
-            sys.exit(1)
-        else:
-            time_filter_length = arg[1]
-    else:
-        # handle generic args like user,
-        arg = sys.argv[current_arg_index]
-        if arg.startswith('--') and '=' in arg:
-            print(f"Processing positional arg: {arg}")
-            arg = arg.split('=')
-            arg_name = arg[0][2:]
-            if not arg_name in allowed_args:
-                print(f"Argument not implemented: {arg}. Exiting.")
-                sys.exit(1)
-            command_line_filters[arg_name] = arg[1]
-        else:
-            print(f"Unrecognized argument: {arg}. Exiting.")
-            sys.exit(1)
-    current_arg_index += 1
-
-if time_filter_length == None:
+# Check command-line arguments #################################################
+if args.time[0] == 'none':
     time_filter = ''
+elif args.time[0].endswith('d') or args.time[0].endswith('h'):
+    time_filter = f"WHERE time >= now() - {args.time[0]} AND time <= now()"
 else:
-    time_filter = f"WHERE time >= now() - {time_filter_length} AND time <= now()"
+    print(f"Unrecognized time parameter: {args.time[0]}.")
+    print(f"This program allows hours or days to be specified as '1h' or '1d' for one hour or day, respectively.")
+    sys.exit(1)
+
+if args.machine[0] == 'unknown' and 'RGT_MACHINE_NAME' in os.environ:
+    print(f"Machine name not specified on command line, found {os.environ['RGT_MACHINE_NAME']} in environment")
+    args.machine[0] = os.environ['RGT_MACHINE_NAME']
+elif args.machine[0] == 'unknown':
+    print(f"Cannot detect machine name, and machine name not provided on command-line. Exiting.")
+    sys.exit(1)
 
 ################################################################################
 
@@ -94,14 +70,6 @@ failed_job_event_value = '2'   # This is the status logged for the event
 success_job_event_name = 'check_end'  # Event name to log under for a completed job
 success_job_event_value = '0'   # This is the status logged for the completed event
 
-if 'RGT_SYSTEM_NAME' in os.environ and current_machine == 'unknown':
-    current_machine = os.environ['RGT_SYSTEM_NAME']
-    print(f"Current machine identified as {current_machine} by RGT_SYSTEM_NAME")
-elif current_machine == 'unknown':
-    print(f"This script requires RGT_SYSTEM_NAME to be set, or --machine=<machine> on the command-line.")
-    sys.exit(1)
-
-
 job_state_codes = {
     'fail': [ 'BOOT_FAIL', 'DEADLINE', 'FAILED', 'NODE_FAIL', 'OUT_OF_MEMORY', 'REVOKED', 'TIMEOUT' ],
     'success': ['COMPLETED'],
@@ -109,6 +77,35 @@ job_state_codes = {
 }
 ################################################################################
 
+def check_data(entry):
+    # Check if all required columns exist ######################################
+    missing_entries = []
+    for req in required_entries:
+        if not req in entry:
+            missing_entries.append(req)
+    if len(missing_entries) > 0:
+        return [ False, f"Missing entries: {','.join(missing_entries)}" ]
+    # Check if the user matches ################################################
+    if not entry['username'] == args.user[0]:
+        return [ False,
+            f"Username of record ({entry['username']}) does not match requested user ({args.user[0]})."]
+    # Check if the app matches ################################################
+    if args.app and not entry['app'] == args.app[0]:
+        return [ False,
+            f"App of record ({entry['app']}) does not match requested app ({args.app[0]})."]
+    # Check if the runtag matches ##############################################
+    if args.runtag and not entry['runtag'] == args.runtag[0]:
+        return [ False,
+            f"Runtag of record ({entry['runtag']}) does not match requested runtag ({args.runtag[0]})."]
+    # Check if the test matches ##################################################
+    if args.test and not entry['test'] == args.test[0]:
+        return [ False,
+            f"Test of record ({entry['test']}) does not match requested test ({args.test[0]})."]
+    if entry['slurm_jobid'] == '[NO_VALUE]':
+        return [ False,
+            f"SLURM job ID is [NO_VALUE]."]
+    # End checks
+    return [True, '']
 
 def query_influx_running():
     """
@@ -137,10 +134,17 @@ def query_influx_running():
     col_names = resp['results'][0]['series'][0]['columns']
     values = resp['results'][0]['series'][0]['values']
     # Let's do the work of transforming this into a list of dicts
-    ret_data = [ dict() for x in range(0, len(values)) ]
+    ret_data = []
     for entry_index in range(0, len(values)):
+        data_tmp = {}
         for c_index in range(0, len(col_names)):
-            ret_data[entry_index][col_names[c_index]] = values[entry_index][c_index]
+            data_tmp[col_names[c_index]] = values[entry_index][c_index]
+        should_add, reason = check_data(data_tmp)
+        if should_add:
+            ret_data.append(data_tmp)
+        else:
+            jobid = data_tmp['test_id'] if 'test_id' in data_tmp else 'unknown'
+            print(f"Skipping test_id {jobid}. Reason: {reason}")
     return ret_data
 
 def post_update_to_influx(d, failed=True):
@@ -192,34 +196,39 @@ def post_update_to_influx(d, failed=True):
         os.chdir(currentdir)
         return False
 
-def check_job_status(slurm_jobid):
+def check_job_status(slurm_jobid_lst):
     """
         Checks if a Slurm job is running by querying sacct
         Returns [False] (list length=1) or
             [JobID, Elapsed, Start, End, State, ExitCode, Reason, Comment]
     """
     sacct_format = 'JobID,Elapsed,Start,End,State%40,ExitCode,Reason%100,Comment%100'
-    cmd=f'sacct -j {slurm_jobid} --format {sacct_format}'
-    os.system(f'{cmd} 2>&1 | head -n 3 > {slurm_jobid}.tmp.txt')
-    with open(f'{slurm_jobid}.tmp.txt', 'r') as f:
+    cmd=f"sacct -j {','.join(slurm_jobid_lst)} --format {sacct_format}"
+    os.system(f'{cmd} 2>&1 > slurm.jobs.tmp.txt')
+    result = {}  # a dictionary of dictionaries
+    with open(f'slurm.jobs.tmp.txt', 'r') as f:
         line = f.readline()
         labels = line.split()
         comment_line = f.readline()   # line of dashes
         # use the spaces in the comment line to split the next line properly
-        # a space indicates next field
-        line = f.readline()
-        fields = []
-        search_pos = -1
-        for i in range(0, len(labels)):
-            next_space = comment_line.find(' ', search_pos + 1)
-            cur_field = line[search_pos+1:next_space].strip()
-            search_pos = next_space
-            fields.append(cur_field)
-    os.remove(f"{slurm_jobid}.tmp.txt")
-    result = {}
-    result['num_cols'] = len(labels)
-    for i in range(0, len(labels)):
-        result[labels[i].lower()] = fields[i]
+        for line in f:
+            fields = {}
+            search_pos = -1
+            for i in range(0, len(labels)):
+                next_space = comment_line.find(' ', search_pos + 1)
+                # No more spaces found, and it's the last label
+                if next_space < 0 and i == len(labels) - 1:
+                    next_space = len(line)
+                elif next_space < 0:
+                    print(f"Couldn't find enough spaces to correctly parse the columns to fit the labels {','.join(labels)}")
+                cur_field = line[search_pos+1:next_space].strip()
+                search_pos = next_space
+                fields[labels[i].lower()] = cur_field
+            if not 'jobid' in fields:
+                print(f"Couldn't find JobID in sacct record. Skipping")
+                continue
+            result[fields['jobid']] = fields
+    os.remove(f"slurm.jobs.tmp.txt")
     return result
 
 def get_user_from_id(user_id):
@@ -234,86 +243,55 @@ def get_user_from_id(user_id):
     os.remove(f"tmp.user.txt")
     return user_name
 
-def check_data(entry):
-    # Check if all required columns exist ######################################
-    missing_entries = []
-    for req in required_entries:
-        if not req in entry:
-            missing_entries.append(req)
-    if len(missing_entries) > 0:
-        return [ False, f"Missing entries: {','.join(missing_entries)}" ]
-    # Check if the user matches ################################################
-    if not entry['username'] == command_line_filters['user']:
-        return [ False,
-            f"Username of record ({entry['username']}) does not match requested user ({command_line_filters['user']})."]
-    # Check if the app matches ################################################
-    if 'app' in command_line_filters and not entry['app'] == command_line_filters['app']:
-        return [ False,
-            f"App of record ({entry['app']}) does not match requested app ({command_line_filters['app']})."]
-    # Check if the runtag matches ##############################################
-    if 'runtag' in command_line_filters and not entry['runtag'] == command_line_filters['runtag']:
-        return [ False,
-            f"Runtag of record ({entry['runtag']}) does not match requested runtag ({command_line_filters['runtag']})."]
-    # Check if the test matches ##################################################
-    if 'test' in command_line_filters and not entry['test'] == command_line_filters['test']:
-        return [ False,
-            f"Test of record ({entry['test']}) does not match requested test ({command_line_filters['test']})."]
-    # End checks
-    return [True, '']
 
 
 data = query_influx_running()
+# This is safe, since each entry has already been checked for slurm_jobid
+slurm_job_ids = [ e['slurm_jobid'] for e in data ]
+
+slurm_data = check_job_status(slurm_job_ids)
 
 for entry in data:
     # check to see if this entry should be parsed
-    should_check, reason = check_data(entry)
-    if not should_check:
-        print(f"Skipping {entry['test_id']}. {reason}.")
-        continue
-
     print(f"Processing {entry['test_id']}, SLURM id {entry['slurm_jobid']} ========================")
-    job_status = check_job_status(entry['slurm_jobid'])
-
-    if not job_status['num_cols'] == 8:
-        print(f"Invalid query returned no data for job {entry['slurm_jobid']}. {job_status['num_cols']} columns. Skipping.")
-        continue
-
     d = {
         'test_id': entry['test_id'],
-        'machine': entry['machine'],
-        'app': entry['app'],
+        'job_id': entry['slurm_jobid'],
         'test': entry['test'],
+        'app': entry['app'],
         'runtag': entry['runtag'],
-        'job_id': entry['slurm_jobid']
+        'machine': entry['machine']
     }
-
-    if job_status['state'] in job_state_codes['fail']:
-        print(f"Found failed job {d['job_id']}. Sending status to Influx.")
-        d['timestamp'] = job_status['end']
-        d['reason'] = f"Job exited in state {job_status['state']} at {job_status['end']}, after running for {job_status['elapsed']}."
-        d['reason'] += f" Exit code: {job_status['exitcode']}, reason: {job_status['reason']}."
-        d['comment'] = job_status['comment']
+    if not entry['slurm_jobid'] in slurm_data:
+        print(f"Can't find data for {entry['slurm_jobid']} in sacct data.")
+        continue
+    if slurm_data[entry['slurm_jobid']]['state'] in job_state_codes['fail']:
+        print(f"Found failed job {entry['slurm_jobid']}. Sending status to Influx.")
+        d['timestamp'] = slurm_data[entry['slurm_jobid']]['end']
+        d['reason'] = f"Job exited in state {slurm_data[entry['slurm_jobid']]['state']} at {slurm_data[entry['slurm_jobid']]['end']}, after running for {slurm_data[entry['slurm_jobid']]['elapsed']}."
+        d['reason'] += f" Exit code: {slurm_data[entry['slurm_jobid']]['exitcode']}, reason: {slurm_data[entry['slurm_jobid']]['reason']}."
+        d['comment'] = slurm_data[entry['slurm_jobid']]['comment']
         post_update_to_influx(d)
-    elif job_status['state'].startswith('CANCELLED'):
+    elif slurm_data[entry['slurm_jobid']]['state'].startswith('CANCELLED'):
         print(f"Found user-cancelled job {d['job_id']}. Sending status to Influx.")
-        d['timestamp'] = job_status['end']
-        d['comment'] = job_status['comment']
-        job_status_long = job_status['state'].split(' ')
+        d['timestamp'] = slurm_data[entry['slurm_jobid']]['end']
+        d['comment'] = slurm_data[entry['slurm_jobid']]['comment']
+        job_status_long = slurm_data[entry['slurm_jobid']]['state'].split(' ')
         if len(job_status_long) > 1:
             cancel_user = get_user_from_id(job_status_long[2])
-            d['reason'] = f" Job canceled at {job_status['end']} by {cancel_user}"
+            d['reason'] = f" Job canceled at {slurm_data[entry['slurm_jobid']]['end']} by {cancel_user}"
         else:
-            d['reason'] = f"Job cancelled at {job_status['end']}"
-        d['reason'] += f", after running for {job_status['elapsed']}."
-        d['reason'] += f" Exit code: {job_status['exitcode']}, reason: {job_status['reason']}."
+            d['reason'] = f"Job cancelled at {slurm_data[entry['slurm_jobid']]['end']}"
+        d['reason'] += f", after running for {slurm_data[entry['slurm_jobid']]['elapsed']}."
+        d['reason'] += f" Exit code: {slurm_data[entry['slurm_jobid']]['exitcode']}, reason: {slurm_data[entry['slurm_jobid']]['reason']}."
         post_update_to_influx(d)
-    elif job_status['state'] in job_state_codes['success']:
+    elif slurm_data[entry['slurm_jobid']]['state'] in job_state_codes['success']:
         print(f"Marking job as completed: {d['job_id']}")
-        d['timestamp'] = job_status['end']
+        d['timestamp'] = slurm_data[entry['slurm_jobid']]['end']
         post_update_to_influx(d, failed = False)
-    elif job_status['state'] in job_state_codes['pending']:
+    elif slurm_data[entry['slurm_jobid']]['state'] in job_state_codes['pending']:
         print(f"Job {d['job_id']} is still pending. No action is being taken.")
     else:
-        print(f"Unrecognized job state: {job_status['state']}. No action is being taken.")
+        print(f"Unrecognized job state: {slurm_data[entry['slurm_jobid']]['state']}. No action is being taken.")
 
 sys.exit(0)
