029b54d7f89ea6011762a97ccea945b2e128be65
Nick Hagerty <hagertynl@ornl.gov>
2022-11-14 16:03:09 -0500

    Added verbosity flag

--- harness/utilities/update_runs_from_slurm.py
+++ harness/utilities/update_runs_from_slurm.py
@@ -2,19 +2,17 @@
 
 ################################################################################
 # Author: Nick Hagerty
-# Date modified: 08-25-2022
+# Date modified: 11-14-2022
 ################################################################################
 # Purpose:
 #   Only intended for SLURM systems.
 #   Queries InfluxDB to get the runs listed as currently running. Then,
 #   queries SLURM to find out if the job crashed or not. POSTs the update
-#   back to Influx under 'check_end' status. Failed jobs report 2, successful
-#   report 0.
+#   back to Influx under 'check_end' status.
 ################################################################################
 ################################################################################
 
 import os
-import sys
 import requests
 import subprocess
 import urllib.parse
@@ -36,6 +34,7 @@ parser.add_argument('--app', nargs=1, action='store', help="Specifies the app to
 parser.add_argument('--test', nargs=1, action='store', help="Specifies the test to update jobs for.")
 parser.add_argument('--runtag', nargs=1, action='store', help="Specifies the runtag to update jobs for.")
 parser.add_argument('--db', nargs=1, default=['dev'], action='store', help="InfluxDB instance name to log to.")
+parser.add_argument('--verbosity', '-v', nargs=1, default=0, type=int, action='store', help="InfluxDB instance name to log to.")
 parser.add_argument('--dry-run', action='store_true', help="When set, prints messages to send to Influx, but does not send them.")
 ################################################################################
 
@@ -49,16 +48,16 @@ args = parser.parse_args()
 # Set up URIs and Tokens #######################################################
 if not args.db[0] in influx_keys.keys():
     print(f"Unknown database version: {args.db[0]} not found in influx_keys. Aborting.")
-    sys.exit(1)
+    exit(1)
 elif not 'POST' in influx_keys[args.db[0]]:
     print(f"POST URL not found in influx_keys[{args.db[0]}]. Aborting.")
-    sys.exit(1)
+    exit(1)
 elif not 'GET' in influx_keys[args.db[0]]:
     print(f"GET URL not found in influx_keys[{args.db[0]}]. Aborting.")
-    sys.exit(1)
+    exit(1)
 elif not 'token' in influx_keys[args.db[0]]:
     print(f"Influx token not found in influx_keys[{args.db[0]}]. Aborting.")
-    sys.exit(1)
+    exit(1)
 
 # Checking succeeded - global setup of URIs and tokens
 post_influx_uri = influx_keys[args.db[0]]['POST']
@@ -73,7 +72,7 @@ elif args.time[0].endswith('d') or args.time[0].endswith('h'):
 else:
     print(f"Unrecognized time parameter: {args.time[0]}.")
     print(f"This program allows hours or days to be specified as '1h' or '1d' for one hour or day, respectively.")
-    sys.exit(1)
+    exit(1)
 
 ################################################################################
 
@@ -94,11 +93,15 @@ state_to_value = {
 job_state_codes = {
     'fail': ['DEADLINE', 'FAILED', 'OUT_OF_MEMORY', 'REVOKED'],
     'timeout': ['TIMEOUT'],
-    'node_fail': ['NODE_FAIL', 'BOOT_FAIL'],
+    'node_fail': ['NODE_FAIL', 'BOOT_FAIL', 'RESIZING'],
     'success': ['COMPLETED'],
     'pending': ['PENDING', 'RUNNING']
 }
 ################################################################################
+
+def print_debug(lvl, msg):
+    if int(lvl) <= int(args.verbosity):
+        print(f"{lvl}: {msg}")
 
 def check_data(entry):
     # Check if all required columns exist ######################################
@@ -141,17 +144,20 @@ def query_influx_running():
     url = f"{get_influx_uri}&db=accept&q={urllib.parse.quote(running_query)}"
     try:
         r = requests.get(url, headers=headers, params={'q': 'requests+language:python'})
+        if int(r.status_code) >= 400:
+            print_debug(0, f"Influx request failed, status_code = {r.status_code}, text = {r.text}, reason = {r.reason}.")
+            exit(1)
     except requests.exceptions.ConnectionError as e:
-        print("InfluxDB is not reachable. Request not sent.")
-        print(str(e))
-        sys.exit(1)
+        print_debug(0, "InfluxDB is not reachable. Request not sent.")
+        print_debug(0, str(e))
+        exit(1)
     except Exception as e:
-        print(f"Failed to send to {url}:")
-        print(str(e))
-        sys.exit(2)
+        print_debug(0, f"Failed to send to {url}:")
+        print_debug(0, str(e))
+        exit(2)
     resp = r.json()
     if not 'series' in resp['results'][0]:
-        print(f"No Running tests found. Full response: {resp}")
+        print_debug(1, f"No Running tests found. Full response: {resp}")
         return []
     # each entry in series is a record
     col_names = resp['results'][0]['series'][0]['columns']
@@ -167,7 +173,7 @@ def query_influx_running():
             ret_data.append(data_tmp)
         else:
             jobid = data_tmp['test_id'] if 'test_id' in data_tmp else 'unknown'
-            print(f"Skipping test_id {jobid}. Reason: {reason}")
+            print_debug(1, f"Skipping test_id {jobid}. Reason: {reason}")
     return ret_data
 
 def post_update_to_influx(d, state_code):
@@ -194,21 +200,21 @@ def post_update_to_influx(d, state_code):
         if not args.dry_run:
             r = requests.post(post_influx_uri, data=influx_event_record_string, headers=headers)
             if int(r.status_code) < 400:
-                print(f"Successfully updated {d['test_id']} with {influx_event_record_string}.")
+                print_debug(1, f"Successfully updated {d['test_id']} with {influx_event_record_string}.")
                 return True
             else:
-                print(f"Influx returned status code: {r.status_code} in response to data: {influx_event_record_string}")
-                print(r.text)
+                print_debug(0, f"Influx returned status code: {r.status_code} in response to data: {influx_event_record_string}")
+                print_debug(1, r.text)
                 return False
         else:
-            print(f"Dry run set. Message: {influx_event_record_string}")
+            print_debug(0, f"Dry run set. Message: {influx_event_record_string}")
     except requests.exceptions.ConnectionError as e:
-        print(f"InfluxDB is not reachable. Request not sent: {influx_event_record_string}")
+        print_debug(0, f"InfluxDB is not reachable. Request not sent: {influx_event_record_string}")
         return False
     except Exception as e:
         # TODO: add more graceful handling of unreachable influx servers
-        print(f"Failed to send {influx_event_record_string} to {influx_url}:")
-        print(e)
+        print_debug(0, f"Failed to send {influx_event_record_string} to {influx_url}:")
+        print_debug(1, e)
         return False
 
 def check_job_status(slurm_jobid_lst):
@@ -221,7 +227,8 @@ def check_job_status(slurm_jobid_lst):
     low_limit = 0
     high_limit = 0
     batch_size = 100
-    print(f"Querying sacct with {len(slurm_jobid_lst)} jobs in batches of up to {batch_size}")
+    node_fail_jobids = []
+    print_debug(1, f"Querying sacct with {len(slurm_jobid_lst)} jobs in batches of up to {batch_size}")
     # Batched into batches of 100
     while low_limit < len(slurm_jobid_lst):
         high_limit = min(low_limit + batch_size, len(slurm_jobid_lst))
@@ -242,13 +249,22 @@ def check_job_status(slurm_jobid_lst):
                     if next_space < 0 and i == len(labels) - 1:
                         next_space = len(line)
                     elif next_space < 0:
-                        print(f"Couldn't find enough spaces to correctly parse the columns to fit the labels {','.join(labels)}")
+                        print_debug(1, f"Couldn't find enough spaces to correctly parse the columns to fit the labels {','.join(labels)}")
                     cur_field = line[search_pos+1:next_space].strip()
                     search_pos = next_space
                     fields[labels[i].lower()] = cur_field
                 if not 'jobid' in fields:
-                    print(f"Couldn't find JobID in sacct record. Skipping")
+                    print_debug(0, f"Couldn't find JobID in sacct record. Skipping")
                     continue
+                elif fields['state'] == 'RESIZING':
+                    print_debug(1, f"Detected RESIZING for job {fields['jobid']}. RESIZING is from node failure + SLURM '--no-kill'. There should be another record in sacct for this job. Skipping")
+                    # Add a field to this job that shows it had/survived a node failure
+                    node_failed_jobids.append(fields['jobid'])
+                    continue
+                elif fields['jobid'] in node_failed_jobids:
+                    # Check if a previous step had come in with RESIZING
+                    print_debug(2, f"Found jobid in node failure list: {fields['jobid']}")
+                    fields['node-failed'] = True
                 result[fields['jobid']] = fields
         os.remove(f"slurm.jobs.tmp.txt")
         low_limit = high_limit  # prepare for next iteration
@@ -261,7 +277,7 @@ def get_user_from_id(user_id):
     with open(f'tmp.user.txt', 'r') as f:
         user_name = f.readline().strip()
     if len(user_name) == 0:
-        print(f"Couldn't find user name for {user_id}. Returning 'unknown'")
+        print_debug(1, f"Couldn't find user name for {user_id}. Returning 'unknown'")
         user_name = 'unknown'
     os.remove(f"tmp.user.txt")
     return user_name
@@ -271,28 +287,32 @@ data = query_influx_running()
 # This is safe, since each entry has already been checked for slurm_jobid
 slurm_job_ids = [ e['slurm_jobid'] for e in data ]
 
+# A job ID will have a field named `node-failed` = True if it survived a node failure via --no-kill
 slurm_data = check_job_status(slurm_job_ids)
+
+skipped = 0
 
 for entry in data:
     # check to see if this entry should be parsed
-    print(f"Processing {entry['test_id']}, SLURM id {entry['slurm_jobid']} ========================")
+    print_debug(1, f"Processing {entry['test_id']}, SLURM id {entry['slurm_jobid']} ========================")
     d = {
         'job_id': entry['slurm_jobid']
     }
     for t in StatusFile.INFLUX_TAGS:
         d[t] = entry[t]
     if not entry['slurm_jobid'] in slurm_data:
-        print(f"Can't find data for {entry['slurm_jobid']} in sacct data. Skipping")
+        print_debug(1, f"Can't find data for {entry['slurm_jobid']} in sacct data. Skipping")
+        skipped += 1
         continue
     if slurm_data[entry['slurm_jobid']]['state'] in job_state_codes['fail']:
-        print(f"Found failed job {entry['slurm_jobid']}. Sending status to Influx.")
+        print_debug(1, f"Found failed job {entry['slurm_jobid']}. Sending status to Influx.")
         d['timestamp'] = slurm_data[entry['slurm_jobid']]['end']
         d['reason'] = f"Job exited in state {slurm_data[entry['slurm_jobid']]['state']} at {slurm_data[entry['slurm_jobid']]['end']}, after running for {slurm_data[entry['slurm_jobid']]['elapsed']}."
         d['reason'] += f" Exit code: {slurm_data[entry['slurm_jobid']]['exitcode']}, reason: {slurm_data[entry['slurm_jobid']]['reason']}."
         d['comment'] = slurm_data[entry['slurm_jobid']]['comment']
         post_update_to_influx(d, 'fail')
     elif slurm_data[entry['slurm_jobid']]['state'].startswith('CANCELLED'):
-        print(f"Found cancelled job {d['job_id']}. Sending status to Influx.")
+        print_debug(1, f"Found cancelled job {d['job_id']}. Sending status to Influx.")
         d['timestamp'] = slurm_data[entry['slurm_jobid']]['end']
         d['comment'] = slurm_data[entry['slurm_jobid']]['comment']
         # Check if CANCELLED BY ...
@@ -306,22 +326,26 @@ for entry in data:
         d['reason'] += f" Exit code: {slurm_data[entry['slurm_jobid']]['exitcode']}, reason: {slurm_data[entry['slurm_jobid']]['reason']}."
         post_update_to_influx(d, 'fail')
     elif slurm_data[entry['slurm_jobid']]['state'] in job_state_codes['node_fail']:
-        print(f"Found node_failure in: {d['job_id']}")
+        print_debug(1, f"Found node_failure in: {d['job_id']}")
         d['timestamp'] = slurm_data[entry['slurm_jobid']]['end']
         d['reason'] = f"Node failure detected. Job exited in state {slurm_data[entry['slurm_jobid']]['state']} at {slurm_data[entry['slurm_jobid']]['end']}, after running for {slurm_data[entry['slurm_jobid']]['elapsed']}."
         post_update_to_influx(d, 'node_fail')
     elif slurm_data[entry['slurm_jobid']]['state'] in job_state_codes['timeout']:
-        print(f"Found timed out job: {d['job_id']}")
+        print_debug(1, f"Found timed out job: {d['job_id']}")
         d['timestamp'] = slurm_data[entry['slurm_jobid']]['end']
         d['reason'] = f"TIMEOUT detected. Job exited in state {slurm_data[entry['slurm_jobid']]['state']} at {slurm_data[entry['slurm_jobid']]['end']}, after running for {slurm_data[entry['slurm_jobid']]['elapsed']}."
         post_update_to_influx(d, 'timeout')
     elif slurm_data[entry['slurm_jobid']]['state'] in job_state_codes['success']:
-        print(f"Marking job as completed: {d['job_id']}")
+        print_debug(1, f"Marking job as completed: {d['job_id']}")
         d['timestamp'] = slurm_data[entry['slurm_jobid']]['end']
         post_update_to_influx(d, 'success')
     elif slurm_data[entry['slurm_jobid']]['state'] in job_state_codes['pending']:
-        print(f"Job {d['job_id']} is still pending. No action is being taken.")
+        printi_debug(1, f"Job {d['job_id']} is still pending. No action is being taken.")
+        skipped += 1
     else:
-        print(f"Unrecognized job state: {slurm_data[entry['slurm_jobid']]['state']}. No action is being taken.")
+        print_debug(0, f"Unrecognized job state: {slurm_data[entry['slurm_jobid']]['state']}. No action is being taken.")
+        skipped += 1
 
-sys.exit(0)
+
+print(f"{len(data) - skipped} entries sent to InfluxDB. {skipped} skipped.")
+exit(0)
