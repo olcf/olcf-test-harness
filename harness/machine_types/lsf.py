5feb61a2617c845633c6a4cf1f34b69bf21e4849
Dan Dietz <ddietz@login2.summit.olcf.ornl.gov>
2023-05-10 15:46:35 -0400

    Apply fixes to gracefully handle bsub/qsub errors

--- harness/machine_types/lsf.py
+++ harness/machine_types/lsf.py
@@ -69,17 +69,11 @@ class LSF(BaseScheduler):
         records = submit_stdout.readlines()
         submit_stdout.close()
 
-        #print("records = ")
-        #print(records)
-
-        #print("Extracting LSF jobID from LSF class")
-        jobid_pattern = re.compile('\d+')
-        #print("jobid_pattern = ")
-        #print(jobid_pattern)
-        #print("jobid_pattern.findall = ")
-        jobid = jobid_pattern.findall(records[0])[0]
-        self.set_job_id(jobid)
-        print("LSF jobID = ",self.get_job_id())
+        if p.returncode is 0:
+            jobid_pattern = re.compile('\d+')
+            jobid = jobid_pattern.findall(records[0])[0]
+            self.set_job_id(jobid)
+            print("LSF jobID = ",self.get_job_id())
 
         return p.returncode
 
