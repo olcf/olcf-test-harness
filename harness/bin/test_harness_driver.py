0edaa3fd416783dad818fcdd7536f09555928dc5
Nick Hagerty <hagertynl@ornl.gov>
2025-02-20 15:34:03 -0500

    Fixed resubmit issue

--- harness/bin/test_harness_driver.py
+++ harness/bin/test_harness_driver.py
@@ -100,7 +100,7 @@ def create_parser():
                            help='Control the level of information printed to stdout.')
     my_parser.add_argument('-r', '--resubmit',
                            help='Have the application test batch script resubmit itself, optionally for a total submission count of N. Leave off N for infinite submissions.',
-                           action='store', nargs='?', type=int, const=-1, default=False)
+                           action='store', nargs='?', type=int, const=-1, default=-1)
     my_parser.add_argument('-R', '--run',
                            help='Run the application test batch script (NOTE: for use within a job)',
                            action='store_true')
@@ -339,13 +339,12 @@ def test_harness_driver(argv=None):
 
     resubmit_count = -1 # -1 means resubmit forever until stopped
     if do_submit:
-        if Vargs.resubmit:
-            resubmit_count = int(Vargs.resubmit)
-            if resubmit_count == 0:
-                # end of max_submissions
-                message = 'Resubmit count is 0. Stopping test cycle.\n'
-                print(message)
-                return 0
+        resubmit_count = int(Vargs.resubmit)
+        if resubmit_count == 0:
+            # end of max_submissions
+            message = 'Resubmit count is 0. Stopping test cycle.\n'
+            print(message)
+            return 0
 
     actions = {
         'build'    : do_build,
