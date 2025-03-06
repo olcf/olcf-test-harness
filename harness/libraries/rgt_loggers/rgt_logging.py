75d5cab96f1bef1b27808497b377b970af3718a9
Dan Dietz <ddietz@login13.frontier.olcf.ornl.gov>
2025-03-06 16:54:17 -0500

    Fix logic bug

--- harness/libraries/rgt_loggers/rgt_logging.py
+++ harness/libraries/rgt_loggers/rgt_logging.py
@@ -75,14 +75,17 @@ class rgt_logger:
         return self.__ch_threshold_level
 
     def getLocation(self, message):
-        loc=""
         if message != "" and self.get_logger_threshold_level() == "DEBUG":
+            loc=""
             stack = inspect.stack()
+            # Step up stack until we are out of the logging bits
             i = 2
             while stack[i][3] == stack[1][3] and i < len(stack):
                 i += 1
             loc = "In function " + stack[i][3] + ": " + message
-        return loc
+            return loc
+        else:
+            return message
 
     def doDebugLogging(self,
                       message):
