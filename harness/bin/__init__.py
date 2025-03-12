324fe2f125d75ba1f24adc2568fb8a53628269e5
Dan Dietz <ddietz@login05.frontier.olcf.ornl.gov>
2025-03-12 09:57:41 -0400

    Restore bash script and update init.py

--- harness/bin/__init__.py
+++ harness/bin/__init__.py
@@ -1,12 +1,7 @@
 __all__ = [
             "check_executable_driver",
-            "filelock",
-            "rgt_calculate_percentages",
-            "parse_test_status",
             "test_harness_driver",
-            "runtests",
-            "record_application_metric",
-            "create_alt_config_file"
+            "log_binary_execution_time",
           ]
 
 version = 2.0
