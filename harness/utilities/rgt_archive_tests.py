1400f917f57496a43c38e7f622ad77f5cb24f82e
Nick Hagerty <hagertynl@ornl.gov>
2025-03-21 14:44:20 -0400

    Added bulk of functionality, include copying to archive, force removal of existing test in archive, and copying common files

--- harness/utilities/rgt_archive_tests.py
+++ harness/utilities/rgt_archive_tests.py
@@ -13,6 +13,8 @@ from datetime import datetime
 import os
 import argparse
 import re
+import shutil
+import tarfile
 
 # For directory names
 from libraries.layout_of_apps_directory import apptest_layout
@@ -22,9 +24,9 @@ from libraries.status_file import StatusFile, get_status_info_from_file
 from libraries.rgt_loggers import rgt_logger_factory
 
 # define some constants
-ALWAYS = 'ALWAYS'
-NEVER = 'NEVER'
-ON_FAIL = 'ON_FAIL'
+KW_ALWAYS = 'ALWAYS'
+KW_NEVER = 'NEVER'
+KW_ON_FAIL = 'ON_FAIL'
 
 def initialize_parser():
     # Initialize argparse ##########################################################
@@ -40,8 +42,8 @@ def initialize_parser():
     parser.add_argument('--endtime', type=str, action='store', help="Absolute end time. Format: YYYY-MM-DDTHH:MM:SSZ. Should only be used with --starttime.")
 
     # Optional customization of preserving/removal behavior
-    parser.add_argument('--keep-workdir', default=ON_FAIL, choices=[ON_FAIL, ALWAYS, NEVER], type=str, action='store', help="Customize when to copy the work directory to archive (default: ON_FAIL).")
-    parser.add_argument('--keep-builddir', default=ON_FAIL, choices=[ON_FAIL, ALWAYS, NEVER], type=str, action='store', help="Customize when to copy the build directory to archive (default: ON_FAIL).")
+    parser.add_argument('--keep-workdir', default=KW_ON_FAIL, choices=[KW_ON_FAIL, KW_ALWAYS, KW_NEVER], type=str, action='store', help=f"Customize when to copy the work directory to archive (default: {KW_ON_FAIL}).")
+    parser.add_argument('--keep-builddir', default=KW_ON_FAIL, choices=[KW_ON_FAIL, KW_ALWAYS, KW_NEVER], type=str, action='store', help=f"Customize when to copy the build directory to archive (default: {KW_ON_FAIL}).")
     parser.add_argument('--delete-scratch-dir', action='store_true', help="DANGEROUS. If set, deletes the build and work directories after archiving.")
     parser.add_argument('--delete-run-dir', action='store_true', help="DANGEROUS. If set, deletes the Run_Archive and Status directories after archiving.")
 
@@ -192,22 +194,125 @@ def should_archive_test(test_path, test_id):
 
 def archive_test(apptest, test_id):
     """ Archives a test_id to the args.path_to_archive argument """
-    archive_file_name = os.path.join(args.path_to_archive, apptest, 'Run_Archive', test_id)
+
+    # Read the latest status file for this test to get machine, runtag, and user info
+    test_archive_dir = os.path.join(args.path_to_archive, apptest, 'Archive', test_id)
     test_run_archive = os.path.join(args.path_to_tests, apptest, apptest_layout.test_run_archive_dirname, test_id)
     test_status = os.path.join(args.path_to_tests, apptest, apptest_layout.test_status_dirname, test_id)
-    # check for existing archive:
-    if os.path.isdir(archive_file_name):
-        logger.doWarningLogging(f"Found {apptest}/{test_id} in archive already at {archive_file_name}. Skipping.")
-        return False
-    elif os.path.isdir(f'{archive_file_name}.tar.gz'):
-        logger.doWarningLogging(f"Found a compressed {apptest}/{test_id} in archive already at {archive_file_name}.tar.gz. Skipping.")
-        return False
 
+    latest_status_file = None
+    current_event_num = 0
+    status_file_regex = re.compile('^Event_[0-9]+_.*\.txt$')
+
+    for status_file_name in os.listdir(test_status):
+        if not status_file_regex.match(status_file_name):
+            continue
+        event_number = int(status_file_name.split('_')[1]) # used to sort if this is a newer event than current
+        if event_number > current_event_num:
+            # Then get the info from the status file & log it to the database
+            latest_status_file = status_file_name
+            current_event_num = event_number
+    
+    logger.doDebugLogging(f"Using status file {test_status}/{status_file_name}")
+    # status file used for exit codes and build_directory and workdir paths
+    test_info = get_status_info_from_file(os.path.join(test_status, status_file_name))
+
+    # check for existing archive:
+    if os.path.isdir(test_archive_dir):
+        if not args.force:
+            logger.doWarningLogging(f"Found {apptest}/{test_id} in archive already at {test_archive_dir}. Skipping.")
+            return False
+        else:
+            logger.doWarningLogging(f"Found {apptest}/{test_id} in archive already at {test_archive_dir}. --force is set, so removing this directory.")
+            shutil.rmtree(test_archive_dir)
+    elif os.path.exists(f'{test_archive_dir}.tar.gz'):
+        if not args.force:
+            logger.doWarningLogging(f"Found a compressed {apptest}/{test_id} in archive already at {test_archive_dir}.tar.gz. Skipping.")
+            return False
+        else:
+            logger.doWarningLogging(f"Found {apptest}/{test_id} in archive already at {test_archive_dir}.tar.gz. --force is set, so removing this tarball.")
+            os.remove(f'{test_archive_dir}.tar.gz')
+
+    # make Archive directory if it doesn't already exist
+    if not os.path.isdir(os.path.dirname(test_archive_dir)):
+        os.makedirs(os.path.dirname(test_archive_dir))
+
+    # Archive -- use shutil.copytree to copy the current Run_Archive with sym-links for build_directory and workdir
+    shutil.copytree(test_run_archive, test_archive_dir, symlinks=True)
+    # remove build_directory and workdir sym-links in Archive
+    os.unlink(os.path.join(test_archive_dir, apptest_layout.test_run_dirname))
+    os.unlink(os.path.join(test_archive_dir, apptest_layout.test_build_dirname))
+    # copy the real build_directory and workdir directories if they exist & if flags are right
+    test_failed = False
+    if (not test_info['event_value'] == '0') and (not test_info['event_name'] == 'job_queued') and (not test_info['event_name'] == 'submit_end'):
+        test_failed = True
+
+    # handle build_directory copying
+    if (args.keep_builddir == KW_ALWAYS) or (args.keep_builddir == KW_ON_FAIL and test_failed):
+        if os.path.isdir(test_info['build_directory']):
+            shutil.copytree(test_info['build_directory'], os.path.join(test_archive_dir, apptest_layout.test_build_dirname), symlinks=True)
+        else:
+            logger.doInfoLogging(f"Build directory for {apptest}/{test_id} does not exist in {test_info['build_directory']}. Skipping copying build_directory.")
+
+    # handle build_directory copying
+    if (args.keep_workdir == KW_ALWAYS) or (args.keep_workdir == KW_ON_FAIL and test_failed):
+        if os.path.isdir(test_info['workdir']):
+            shutil.copytree(test_info['workdir'], os.path.join(test_archive_dir, apptest_layout.test_run_dirname), symlinks=True)
+        else:
+            logger.doInfoLogging(f"Work directory for {apptest}/{test_id} does not exist in {test_info['workdir']}. Skipping copying workdir.")
+
+    # Compress
+    if args.compress:
+        with tarfile.open(f'{test_archive_dir}.tar.gz', "w:gz") as test_archive_tar:
+            test_archive_tar.add(f'{test_archive_dir}', arcname=f'{test_id}')
+        # Remove existing directory once compression is done
+        shutil.rmtree(test_archive_dir)
+
+    # Clean-up based off command-line flags --delete-scratch-dir and --delete-run-dir
+    if args.delete_scratch_dir and os.path.isdir(test_info['workdir']):
+        logger.doDebugLogging(f"Removing scratch directory for test {apptest}/{test_id}.")
+        # scratch directory for this test is the parent directory of workdir or build_directory
+        shutil.rmtree(os.dirname(test_info['workdir']))
+
+    if args.delete_run_dir:
+        logger.doDebugLogging(f"Removing Run_Archive and Status directories for test {apptest}/{test_id}.")
+        shutil.rmtree(test_run_archive)
+        shutil.rmtree(test_status)
 
     return True
 
 def archive_apptest_common_files(apptest):
     """ Archives app-Source, test-Scripts, and test-Source directories, if they are not already archived """
+
+    test_archive_dir = os.path.join(args.path_to_archive, apptest)
+    if os.path.isdir(os.path.join(test_archive_dir, apptest_layout.test_scripts_dirname)) and \
+        os.path.isdir(os.path.join(os.path.dirname(test_archive_dir), apptest_layout.app_source_dirname)) \
+        and not args.force:
+        return False
+    elif args.force:
+        # then remove any common files, to replace later in this function
+        if os.path.isdir(os.path.join(test_archive_dir, apptest_layout.test_scripts_dirname)):
+            shutil.rmtree(os.path.join(test_archive_dir, apptest_layout.test_scripts_dirname))
+        if os.path.isdir(os.path.join(test_archive_dir, apptest_layout.test_source_dirname)):
+            shutil.rmtree(os.path.join(test_archive_dir, apptest_layout.test_source_dirname))
+        if os.path.isdir(os.path.join(os.path.dirname(test_archive_dir), apptest_layout.app_source_dirname)):
+            shutil.rmtree(os.path.join(os.path.dirname(test_archive_dir), apptest_layout.app_source_dirname))
+
+    apptest_root = os.path.join(args.path_to_tests, apptest)
+    test_scripts = os.path.join(apptest_root, apptest_layout.test_scripts_dirname)
+    test_source = os.path.join(apptest_root, apptest_layout.test_source_dirname)
+    app_source = os.path.join(os.path.dirname(apptest_root), apptest_layout.app_source_dirname)
+
+    # app/Source
+    shutil.copytree(app_source, os.path.join(os.path.dirname(test_archive_dir), apptest_layout.app_source_dirname), symlinks=True)
+
+    # app/test/Source
+    if os.path.isdir(test_source):
+        shutil.copytree(test_source, os.path.join(test_archive_dir, apptest_layout.test_source_dirname), symlinks=True)
+
+    # app/test/Scripts
+    shutil.copytree(test_scripts, os.path.join(test_archive_dir, apptest_layout.test_scripts_dirname), symlinks=True)
+
     return True
 
 # Handle --no-tqdm flag
@@ -228,12 +333,10 @@ total_logged = 0
 limit_reached = False
 
 timestart = datetime.now()
-import time
 
 for apptest in my_apptests_for:
     # We assume that Run_Archive and Status hold the same set of test IDs, and that there is at least 1 test_id in there
     for testid in os.listdir(os.path.join(args.path_to_tests, apptest, apptest_layout.test_run_archive_dirname)):
-        time.sleep(60)
         if not test_id_regex.match(testid):
             logger.doDebugLogging(f"Excluding test ID that does not match regex: {testid}")
         elif should_archive_test(f"{args.path_to_tests}/{apptest}", testid):
