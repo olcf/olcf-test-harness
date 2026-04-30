99292a79a5b9ce4b05eec73c68dca91922c818fa
Nick Hagerty <hagertynl@ornl.gov>
2026-04-30 12:45:06 -0400

    Add LSF submit-via-stdin functionality, made default. RGT_LSF_SUBMIT_AS_STDIN=0 to revert to old behavior

--- harness/machine_types/lsf.py
+++ harness/machine_types/lsf.py
@@ -57,13 +57,14 @@ class LSF(BaseScheduler):
         submit_stdout = open(temp_stdout,"w")
         submit_stderr = open(temp_stderr,"w")
 
-        # We no longer need 'bsub <' since we are using the OLCF bsub
-        #jobfileobj = open(batchfilename,"r")
-        #p = subprocess.Popen(args,stdout=submit_stdout,stderr=submit_stderr,stdin=jobfileobj)
-        #jobfileobj.close()
-
-        p = subprocess.Popen(args,stdout=submit_stdout,stderr=submit_stderr)
-        p.wait()
+        if 'RGT_LSF_SUBMIT_AS_STDIN' in os.environ and \
+                str(os.environ['RGT_LSF_SUBMIT_AS_STDIN']) == '0':
+            p = subprocess.Popen(args,stdout=submit_stdout,stderr=submit_stderr)
+            p.wait()
+        else:
+            with open(batchfilename,"r") as jobfileobj:
+                p = subprocess.Popen(args,stdout=submit_stdout,stderr=submit_stderr,stdin=jobfileobj)
+                p.wait()
 
         submit_stdout.close()
         submit_stderr.close()
