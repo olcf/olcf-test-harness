4b59f27068222b9eaa127c315d8a9c562ecd6b84
Nick Hagerty <hagertynl@ornl.gov>
2026-04-30 15:27:37 -0400

    Changing default bsub command to use stdin instead of batch file name

--- harness/machine_types/lsf.py
+++ harness/machine_types/lsf.py
@@ -47,7 +47,12 @@ class LSF(BaseScheduler):
         elif 'RGT_PROJECT_ID' in os.environ:
             qargs += " -P " + os.environ.get('RGT_PROJECT_ID')
 
-        qcommand = self.__submitCmd + " " + qargs + " " + batchfilename
+        qcommand = self.__submitCmd + " " + qargs
+
+        if 'RGT_LSF_SUBMIT_AS_STDIN' in os.environ and \
+                str(os.environ['RGT_LSF_SUBMIT_AS_STDIN']) == '0':
+            qcommand += " " + batchfilename
+
         self.__logger.doInfoLogging(f"{qcommand}")
 
         args = shlex.split(qcommand)
